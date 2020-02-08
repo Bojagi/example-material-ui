36836463c086dfca384b3d8ff674854f07258908
JohannesMerz <johannes_merz@gmx.de>
2020-02-08 12:23:45 +0100

    base setup

---
+++ src/components/Button.bojagi.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import Button from './Button'
+
+export const buttonPrimary = () => 
+  <Button variant="contained" color="primary">
+    Primary Contained
+  </Button>
---
+++ src/components/Button.js
@@ -0,0 +1,7 @@
+// @component
+
+import React from 'react';
+import Button from '@material-ui/core/Button';
+
+// we just want to showcase material ui inside bojagi
+export default Button;
