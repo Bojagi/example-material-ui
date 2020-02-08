ed1a5416597b017452eb5434b47f0954fc89fca7
JohannesMerz <johannes_merz@gmx.de>
2020-02-08 12:32:41 +0100

    add more bojagi stories for button

--- src/components/Button.bojagi.js
+++ src/components/Button.bojagi.js
@@ -5,3 +5,8 @@ export const buttonPrimary = () =>
   <Button variant="contained" color="primary">
     Primary Contained
   </Button>
+
+export const buttonSecondary = () =>
+  <Button variant="contained" color="secondary">
+    Secondary Contained
+  </Button>
